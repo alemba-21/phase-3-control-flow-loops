39b93cc40730c604198fcaa99f61d1e2e5fcc71e
alemba-21 <alembakiragu@gmail.com>
2022-08-18 20:05:43 +0300

    Done.

--- looping.rb
+++ looping.rb
@@ -17,8 +17,18 @@ end
 
 def fizzbuzz_printer
   # your code here
+  (1..100).each do |num|
+    puts fizzbuzz(num)
+  end
 end
 
 def reverse_string(str)
   # your code here
+  reversed_string = ""
+  i = 0 
+  while i < str.length
+    reversed_string = str[i] + reversed_string
+    i += 1
+  end
+  reversed_string
 end
